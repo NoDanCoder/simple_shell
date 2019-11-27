de53a55a3e18b936341bc0fa7e3509759cb8ebc1
Alejandro Robayo <1249@holbertonschool.com>
2019-11-27 16:15:26 -0500

    [Fix] messages of help command

--- help1.c
+++ help1.c
@@ -85,15 +85,15 @@ ssize_t _help_cmd(hshpack *shpack)
 		_puts("Shell HSH, version 1.0(1)-release (x86_64-pc-linux-gnu)\n");
 		_puts("These shell commands are defined internally.\n");
 		_puts("Type `help' to see this list.\n");
-		_puts("Type help  'BUILTIN'' to find out more about the function 'BUILTIN'.\n");
+		_puts("Type help  'BUILTIN'' to find out more about the function 'BUILTIN'.\n\n");
 		bcheck = 1;
-		_puts("exit [n]\n");
-		_puts("env\n");
-		_puts("setenv [VARIABLE] [VALUE]\n");
-		_puts("unsetenv [VARIABLE]\n");
-		_puts("cd [DIRECTORY]\n");
-		_puts("help [BUILTIN ...]\n");
-		_puts("alias [name[='value'] ...]\n");
+		_puts(" exit [n]\n");
+		_puts(" env\n");
+		_puts(" setenv [VARIABLE] [VALUE]\n");
+		_puts(" unsetenv [VARIABLE]\n");
+		_puts(" cd [DIRECTORY]\n");
+		_puts(" help [BUILTIN ...]\n");
+		_puts(" alias [name[='value'] ...]\n");
 	}
 	if (bcheck == 0)
 	{
